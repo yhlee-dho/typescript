b4b8fede37430c047f1e6299a3bf091582569a71
Yong Lee <yhlee89.dho@gmail.com>
2021-02-21 12:43:12 -0600

    understand the typescript compiler aka tsc

--- egghead/up-and-running-with-typescript/index.tsx
+++ egghead/up-and-running-with-typescript/index.tsx
@@ -3,10 +3,10 @@ console.log("Hello from TypeScript!")
 import React from "react"
 import { render } from "react-dom"
 
-// const App = () => <div>Hello there General Kenobi</div>
+const App = () => <div>Hello there General Kenobi</div>
 
-class App extends React.Component {
-  render() {}
-}
+// class App extends React.Component {
+//   render() {}
+// }
 
 render(<App />, document.getElementById("app"))
