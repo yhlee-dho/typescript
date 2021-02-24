3974029181b7d7ad6e8dd1ba9c0d321e3d6aacbe
Yong Lee <yhlee89.dho@gmail.com>
2021-02-23 21:10:40 -0600

    1-8

---
+++ egghead/up-and-running-with-typescript/1-8/index.ts
@@ -0,0 +1,15 @@
+// use type definitions for popular projects on npm with @types
+
+import _ from "lodash"
+import $ from "jquery"
+
+// _.uniq([1, 1, 2])
+console.log(_.uniq([1, 1, 2]))
+
+_.camelCase()
+
+const name = $()
+
+if (!$()) {
+  
+}
