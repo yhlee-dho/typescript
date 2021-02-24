8d2153b72b8f36c5dfeb9da3c0e1ec1c681124dd
Yong Lee <yhlee89.dho@gmail.com>
2021-02-23 20:51:02 -0600

    1-6

---
+++ egghead/up-and-running-with-typescript/1-6/index.ts
@@ -0,0 +1,8 @@
+const input = document.getElementById("input") as HTMLInputElement
+
+input.autofocus = true
+
+input.addEventListener("input", event => {
+  const i = event.currentTarget as HTMLInputElement
+  console.log(i.value)
+})
