b94317d045914059aa69b21e74f502c1301bd19b
Yong Lee <yhlee89.dho@gmail.com>
2021-02-25 22:37:31 -0600

    1-3

--- egghead/use-typescript-to-develop-react-applications/1-2/src/app.tsx
+++ egghead/use-typescript-to-develop-react-applications/1-2/src/app.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>Hello General Kenobi</div>
-    )
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>Hello General Kenobi</div>
+//     )
+//   }
+// }
+
+const App = ({ message }) => <div>{ message }</div>
+
 
 ReactDOM.render(
-  <App/>,
+  <App message ="hello world"/>,
   document.getElementById('root')
 )
