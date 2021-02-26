e38a8b50dc8ecaa934e2baae7a9570bd3c512a1c
Yong Lee <yhlee89.dho@gmail.com>
2021-02-25 23:04:05 -0600

    1-4

--- egghead/use-typescript-to-develop-react-applications/1-2/src/app.tsx
+++ egghead/use-typescript-to-develop-react-applications/1-2/src/app.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom';
 
+// const App = ({ message }) => <div>{ message }</div>
+
+// const <App:React.SFC> message: string </App:React.SFC> = ({ message }) => <div>{ message }</div>
+
+
 // class App extends React.Component {
 //   render() {
 //     return (
@@ -9,10 +14,41 @@ import * as ReactDOM from 'react-dom';
 //   }
 // }
 
-const App = ({ message }) => <div>{ message }</div>
+type AppProps = {
+  message: string,
+}
+type AppState = {
+  count: number,
+}
+class App extends React.Component<AppProps, AppState> {
+//   message: string
+// }, {
+//   count: number,
+//   }, {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count +1
+    })
+  }
+  render() {
+    return (
+      // <div>{this.props.message} {this.state.count}</div>
+      <div onClick={this.increment}>{this.props.message}{this.state.count}</div>
+    )
+  }
+
+}
 
 
 ReactDOM.render(
+  // <App/>,
   <App message ="hello world"/>,
+
   document.getElementById('root')
 )
